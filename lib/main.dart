e376f9df8308c2d88a57d1f6d8e6ae923ff30cfa
Fildza Nur Sabrina <fildza.nursabrina@gmail.com>
2022-10-08 10:08:59 +0700

    change login screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter First App'),
+      home: const MyHomePage(title: 'Login Screen'),
     );
   }
 }
